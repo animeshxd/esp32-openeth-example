b20c3b409f671d32172068c6bace3055d97b624c
Animesh Murmu <am2646374@gmail.com>
2025-05-23 21:34:44 +0530

    Fix drive file path in QEMU script

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,4 @@
 qemu-system-xtensa \
     -nographic -machine esp32 \
-    -drive file=build/build/flash_image.bin,if=mtd,format=raw \
+    -drive file=build/flash_image.bin,if=mtd,format=raw \
     -nic tap,model=open_eth,ifname=tap0,script=no,downscript=no
